41dabf1a0b891f7ce0ed70a25e49d0259b19fd93
Ahmed Allam <ahmed.allam@thed.io>
2019-05-28 19:53:36 +0200

    Change selected cell to red in side menu

--- OMG Group/side menu/SideMenuTableViewController.swift
+++ OMG Group/side menu/SideMenuTableViewController.swift
@@ -15,15 +15,39 @@ protocol SideMenuDelegate: class {
 class SideMenuTableViewController: UITableViewController {
     weak var delegate: SideMenuDelegate?
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectCell(at: indexPath)
         delegate?.didSelectItem(at: indexPath.row)
     }
     
-
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        deselectCell(at: indexPath)
+    }
+    
+    private func selectCell(at indexPath: IndexPath ){
+        updateCell(isSelected: true, at: indexPath)
+    }
+    
+    private func deselectCell(at indexPath: IndexPath ){
+        updateCell(isSelected: false, at: indexPath)
+    }
+    
+    private func updateCell(isSelected: Bool, at indexPath: IndexPath){
+        let cell = tableView.cellForRow(at: indexPath)
+        let containerView = cell?.subviews.first?.subviews
+        let iconImageView = containerView?.filter{$0.tag == 0}.first as? UIImageView
+        let label = containerView?.filter{$0.tag == 1}.first as? UILabel
+        let iconName = isSelected ? "red-sidemenu-\(indexPath.row)" : "sidemenu-\(indexPath.row)"
+        iconImageView?.image = UIImage(named: iconName)
+        label?.textColor = isSelected ? UIColor.red : UIColor.white
+        tableView.rectForRow(at: indexPath)
+    }
+    
+    
+    
 }
